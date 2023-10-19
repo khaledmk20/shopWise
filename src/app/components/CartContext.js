b68712ade7426f1d5e96f5f3e4e579f22ed36c24
Khaled Abdelalim <khaled.korany2004@gmail.com>
2023-10-19 17:19:39 +0300

    fix localstorage bug

--- src/app/components/CartContext.js
+++ src/app/components/CartContext.js
@@ -5,16 +5,22 @@ export const CartContext = createContext({});
 export function CartContextProvider({ children }) {
   const ls = typeof window !== "undefined" ? window.localStorage : null;
   const [cartProducts, setCartProducts] = useState([]);
+  const [isEmptyCart, setIsEmptyCart] = useState(true);
+
   useEffect(() => {
     if (cartProducts?.length > 0) {
       ls?.setItem("cart", JSON.stringify(cartProducts));
+    } else {
+      setIsEmptyCart(true);
     }
   }, [cartProducts, ls]);
+
   useEffect(() => {
-    if (ls && ls.getItem("cart")) {
-      setCartProducts(JSON.parse(ls.getItem("cart")));
+    if (isEmptyCart && ls) {
+      ls.removeItem("cart");
     }
-  }, [ls]);
+  }, [isEmptyCart, ls]);
+
   function addProduct(productId) {
     setCartProducts((prev) => [...prev, productId]);
   }
@@ -28,6 +34,7 @@ export function CartContextProvider({ children }) {
       return prev;
     });
   }
+
   function clearCart() {
     setCartProducts([]);
   }
